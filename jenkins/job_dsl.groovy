a2fb99b2b5466446a68536c88cb13e9f72fdff9c
Laurent Enzo <enzo.laurent@epitech.eu>
2023-02-15 16:25:33 +0100

    [UPDATE]: move jenkins files in jenkins folder

---
+++ jenkins/job_dsl.groovy
@@ -0,0 +1,111 @@
+folder('Whanos base images') {
+        displayName('Whanos base images')
+        description('Folder for whanos based images')
+}
+folder('Projects') {
+        displayName('Projects')
+        description('Folder projects')
+}
+
+// fresstyle job linked to based image 
+
+freeStyleJob('/Whanos base images/whanos-c') {
+    description('build the C base image so that it is available for the Jenkins instance host to use.')
+    parameters {
+    }
+    scm {
+        github('https://github.com/EpitechPromo2025/B-DOP-500-NAN-5-1-whanos-enzo.laurent')
+    }
+    steps {
+        sh 'docker build -t whanos-c -f images/c/Dockerfile.base .'
+    }
+}
+freeStyleJob('/Whanos base images/whanos-java') {
+    description('build the Java base image so that it is available for the Jenkins instance host to use.')
+    parameters {
+    }
+    scm {
+        github('https://github.com/EpitechPromo2025/B-DOP-500-NAN-5-1-whanos-enzo.laurent')
+    }
+    steps {
+        sh 'docker build -t whanos-java -f images/java/Dockerfile.base .'
+    }
+}
+freeStyleJob('/Whanos base images/whanos-javascript') {
+    description('build the Javascript base image so that it is available for the Jenkins instance host to use.')
+    parameters {
+    }
+    scm {
+        github('https://github.com/EpitechPromo2025/B-DOP-500-NAN-5-1-whanos-enzo.laurent')
+    }
+    steps {
+        sh 'docker build -t whanos-javascript -f images/javascript/Dockerfile.base .'
+    }
+}
+freeStyleJob('/Whanos base images/whanos-python') {
+    description('build the Python base image so that it is available for the Jenkins instance host to use.')
+    parameters {
+    }
+    scm {
+        github('https://github.com/EpitechPromo2025/B-DOP-500-NAN-5-1-whanos-enzo.laurent')
+    }
+    steps {
+        sh 'docker build -t whanos-python -f images/python/Dockerfile.base .'
+    }
+}
+freeStyleJob('/Whanos base images/whanos-befunge') {
+    description('build the Befunge base image so that it is available for the Jenkins instance host to use.')
+    parameters {
+    }
+    scm {
+        github('https://github.com/EpitechPromo2025/B-DOP-500-NAN-5-1-whanos-enzo.laurent')
+    }
+    steps {
+        sh 'docker build -t whanos-befunge -f images/befunge/Dockerfile.base .'
+    }
+}
+
+freeStyleJob('/Whanos base images/Build all base images') {
+    agent any
+    stages {
+        stage('info') {
+            steps {
+                echo "Build all base image"
+            }
+        }
+        stage('triggerWhanosC') {
+            steps {
+                build job: "/Whanos base images/whanos-c", wait: true
+            }
+        }
+        stage('triggerWhanosJava') {
+            steps {
+                build job: "/Whanos base images/whanos-java", wait: true
+            }
+        }
+        stage('triggerWhanosJavascript') {
+            steps {
+                build job: "/Whanos base images/whanos-javascript", wait: true
+            }
+        }
+        stage('triggerWhanosPython') {
+            steps {
+                build job: "/Whanos base images/whanos-python", wait: true
+            }
+        }
+        stage('triggerWhanosBefunge') {
+            steps {
+                build job: "/Whanos base images/whanos-befunge", wait: true
+            }
+        }
+    }
+}
+
+freeStyleJob('link-project') {
+    description('')
+    parameters {
+    }
+    steps {
+        // create jobs with specifications
+    }
+}
