035c5be130b354d0ed6551dd3b5e1dee6594e7b6
Laurent Enzo <enzo.laurent@epitech.eu>
2023-02-15 17:27:21 +0100

    [UPDATE]: correct jenkins code with the correct function

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,3 @@ COPY jenkins/plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
 COPY images /images
 COPY jenkins /jenkins
-RUN mv /jenkins/job_dsl.groovy /var/lib/docker/volumes/jenkins-data/job_dsl.groovy && /var/lib/docker/volumes/jenkins-data/jenkinsConfig.yml 
--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -12,68 +12,52 @@ folder('Projects') {
 freeStyleJob('/Whanos base images/whanos-c') {
     description('build the C base image so that it is available for the Jenkins instance host to use.')
     steps {
-        sh 'docker build -t whanos-c -f /images/c/Dockerfile.base .'
+        shell('docker build -t whanos-c -f /images/c/Dockerfile.base .')
     }
 }
 freeStyleJob('/Whanos base images/whanos-java') {
     description('build the Java base image so that it is available for the Jenkins instance host to use.')
     steps {
-        sh 'docker build -t whanos-java -f /images/java/Dockerfile.base .'
+        shell('docker build -t whanos-java -f /images/java/Dockerfile.base .')
     }
 }
 freeStyleJob('/Whanos base images/whanos-javascript') {
     description('build the Javascript base image so that it is available for the Jenkins instance host to use.')
     steps {
-        sh 'docker build -t whanos-javascript -f /images/javascript/Dockerfile.base .'
+        shell('docker build -t whanos-javascript -f /images/javascript/Dockerfile.base .')
     }
 }
 freeStyleJob('/Whanos base images/whanos-python') {
     description('build the Python base image so that it is available for the Jenkins instance host to use.')
     steps {
-        sh 'docker build -t whanos-python -f /images/python/Dockerfile.base .'
+        shell('docker build -t whanos-python -f /images/python/Dockerfile.base .')
     }
 }
 freeStyleJob('/Whanos base images/whanos-befunge') {
     description('build the Befunge base image so that it is available for the Jenkins instance host to use.')
     steps {
-        sh 'docker build -t whanos-befunge -f /images/befunge/Dockerfile.base .'
+        shell('docker build -t whanos-befunge -f /images/befunge/Dockerfile.base .')
     }
 }
 
 freeStyleJob('/Whanos base images/Build all base images') {
     description('build all base images so that it is available for the Jenkins instance host to use.')
-    agent any
-    stages {
-        stage('info') {
-            steps {
-                echo "Build all base image"
-            }
-        }
-        stage('triggerWhanosC') {
-            steps {
-                build job: "/Whanos base images/whanos-c", wait: true
-            }
-        }
-        stage('triggerWhanosJava') {
-            steps {
-                build job: "/Whanos base images/whanos-java", wait: true
-            }
-        }
-        stage('triggerWhanosJavascript') {
-            steps {
-                build job: "/Whanos base images/whanos-javascript", wait: true
-            }
-        }
-        stage('triggerWhanosPython') {
-            steps {
-                build job: "/Whanos base images/whanos-python", wait: true
-            }
-        }
-        stage('triggerWhanosBefunge') {
-            steps {
-                build job: "/Whanos base images/whanos-befunge", wait: true
-            }
-        }
+    publishers {
+        downstream (
+            "/Whanos base images/whanos-c"
+        )
+        downstream (
+            "/Whanos base images/whanos-java"
+        )
+        downstream (
+            "/Whanos base images/whanos-javascript"
+        )
+        downstream (
+            "/Whanos base images/whanos-python"
+        )
+        downstream (
+            "/Whanos base images/whanos-befunge"
+        )
     }
 }
 
