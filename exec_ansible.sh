085621cf5eede98398bbee44978b45b8defe0b58
Laurent Enzo <enzo.laurent@epitech.eu>
2023-02-20 18:08:19 +0100

    [ADD]: ansible playbook without k8s deployment for app

---
+++ exec_ansible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -o nounset -o pipefail -o errexit
+
+# Load all variables from .env and export them all for Ansible to read
+set -o allexport
+source ".env"
+set +o allexport
+
+# Run Ansible
+exec ansible-playbook "ansible/azure_deployment.yml"
